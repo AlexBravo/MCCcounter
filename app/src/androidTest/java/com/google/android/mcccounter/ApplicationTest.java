bf12ca38a55d383bc6507010994cc8dfb060309a
Maxim Bravo <maxim@alexbravo.com>
2015-12-12 13:56:20 -0800

    Initial commit

---
+++ app/src/androidTest/java/com/google/android/mcccounter/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.google.android.mcccounter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest{
+    @Test
+    public void mcccounterTest(){
+        assertEquals("hi", "hi");
+    }
+}
---
+++ app/src/main/java/com/google/android/mcccounter/MccCounter.java
@@ -0,0 +1,7 @@
+package com.google.android.mcccounter;
+
+/**
+ * Created by Kids on 12/12/2015.
+ */
+public class MccCounter {
+}
