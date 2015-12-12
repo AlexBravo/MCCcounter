8403be1cc1c3ffbbcffc03a482d7ead407e8d834
Maxim Bravo <maxim@alexbravo.com>
2015-12-12 14:14:53 -0800

    Add 3 test cases.

--- app/src/main/java/com/google/android/mcccounter/MccCounter.java
+++ app/src/main/java/com/google/android/mcccounter/MccCounter.java
@@ -1,7 +1,17 @@
 package com.google.android.mcccounter;
 
-/**
- * Created by Kids on 12/12/2015.
- */
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//Created by Kids on 12/12/2015.
+
 public class MccCounter {
+    public Map<String, Integer> calculateMCCs(String in){
+        Map<String, Integer> mccs = new HashMap<>();
+        if(!in.isEmpty()){
+            mccs.put("ing", 1);
+        }
+        return mccs;
+    }
 }
---
+++ app/src/test/java/com/google/android/mcccounter/MccCounterTest.java
@@ -0,0 +1,37 @@
+package com.google.android.mcccounter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+//Created by Kids on 12/12/2015.
+public class MccCounterTest {
+    private MccCounter mccCounter;
+    @Before
+    public void setUp() throws Exception {
+        mccCounter = new MccCounter();
+    }
+
+    @Test
+    public void testEmptyString() throws Exception {
+        assertEquals(new HashMap<>(), mccCounter.calculateMCCs(""));
+    }
+
+    @Test
+    public void testKing() throws Exception {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("ing", 1);
+        assertEquals(map, mccCounter.calculateMCCs("king"));
+    }
+
+    @Test
+    public void testSinging() throws Exception {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("ing", 2);
+        assertEquals(map, mccCounter.calculateMCCs("singing"));
+    }
+}
